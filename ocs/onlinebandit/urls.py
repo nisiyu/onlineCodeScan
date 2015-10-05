d018d127ab446d9f0a8371877f4f342566147da7
nisiyu <nisiyu91@gmail.com>
2015-10-05 21:34:47 +0800

    upload function: forms upload a file to upload directory

--- ocs/onlinebandit/urls.py
+++ ocs/onlinebandit/urls.py
@@ -7,6 +7,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<user_id>[0-9]+)/$', views.userview, name='detail'),
-    url(r'^(?P<user_id>[0-9]+)/(?P<plan_id>[0-9]+)/$', views.planview, name='detail'),
+    url(r'^(?P<user_id>[0-9]+)/$', views.userview, name='userview'),
+    url(r'^(?P<user_id>[0-9]+)/(?P<plan_id>[0-9]+)/$', views.planview, name='planview'),
+    url(r'^upload/$', views.uploadview, name='uploadview')
 ]
--- ocs/onlinebandit/views.py
+++ ocs/onlinebandit/views.py
@@ -1,12 +1,53 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
+from django.views import generic
+from .models import user,plan,item
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from .forms import UploadFileForm
+from django.views.decorators.csrf import csrf_exempt, csrf_protect
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello,world!")
 
 def userview(request, user_id):
-    return HttpResponse(str(user_id))
+    # return HttpResponse(str(user_id))
+    try:
+        # uobj = user.objects.get(pk=user_id)
+        objs = plan.objects.filter(owner=user_id)[:]
+    except plan.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'usertemplate.html', {'user_plan': objs, 'user_id': user_id, 'request': request})
+
 
 def planview(request, user_id, plan_id):
-    return HttpResponse(str(user_id)+' '+str(plan_id))
+    try:
+        # uobj = user.objects.get(pk=user_id)
+        objs = item.objects.filter(owner=plan_id)[:]
+    except plan.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'usertemplate.html', {'user_plan': objs, 'user_id': user_id})
+
+class DetailView(generic.DetailView):
+    model = plan
+    template_name = 'usertemplate.html'
+
+@csrf_exempt
+def uploadview(request):
+
+    if request.method == 'POST':
+        print request.FILES
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            ## handle uploading
+            pass
+    else:
+        form = UploadFileForm()
+    return render_to_response('uploadtemplate.html', {'form': form})
+
+def handle_uploaded_file(f):
+    with open('dest.zip', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
